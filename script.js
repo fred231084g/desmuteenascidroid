4b019fc493f3e7d5143bcaed43d59f0b8f8bf2c3
Glitch (hello-webpage) <none>
2020-10-01 03:49:31 +0000

    💝🎑 Checkpoint ./index.html:22262291/38:22284543/1077 ./style.css:22284543/302 ./script.js:22262291/109

--- script.js
+++ script.js
@@ -2,4 +2,4 @@
     to your site with Javascript */
 
 // prints "hi" in the browser's dev tools console
-console.log("siganme en github carajo!!!! github.com/elpanajl");
+console.log("visiten mi página carajo https://scidroid.live/");
